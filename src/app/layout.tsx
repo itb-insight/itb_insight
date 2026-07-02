ea46825011658f3be01f47869039e8bf91eccb6f
meleppz <melvin.irsyad2201@gmail.com>
2026-07-02 17:52:07 +0700

    project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { Gabarito } from "next/font/google"
+
+const gabarito = Gabarito({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "900"],
+  variable: "--font-primary",
+})
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={gabarito.variable}>
+      <body>{children}</body>
+    </html>
+  )
+}
