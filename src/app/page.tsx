a5548e22d42784ccad72f8e50f16123b3a1775e4
meleppz <melvin.irsyad2201@gmail.com>
2026-07-08 22:18:07 +0700

    feat: put all sections together

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+import Navbar from "@/shared/components/Navbar/Navbar"
+import HeroSection from "@/features/landing/components/HeroSection/HeroSection"
+import AboutSection from "@/features/landing/components/AboutSection/AboutSection"
+import StatsSection from "@/features/landing/components/StatsSection/StatsSection"
+import GallerySection from "@/features/landing/components/GallerySection/GallerySection"
+import TimelineSection from "@/features/landing/components/TimelineSection/TimelineSection"
+import MediaPartnersSection from "@/features/landing/components/MediaPartnersSection/MediaPartnersSection"
+import Footer from "@/shared/components/Footer/Footer"
+
 export default function HomePage() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <StatsSection />
+      <GallerySection />
+      <TimelineSection />
+      <MediaPartnersSection />
+      <Footer />
+
     </main>
   )
 }
